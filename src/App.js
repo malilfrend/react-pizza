af4c9f3e2e3f85385cfd45b14671cc83a4e9bd63
Aleksey Chebykin <chebykinapwork@gmail.com>
2022-09-12 22:47:50 +0300

    add state to Sort, PizzaBlock

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {pizzas.map((pizza) => {
-              return <PizzaBlock {...pizza} key={Math.random()} />;
+              return <PizzaBlock {...pizza} key={pizza.title} />;
             })}
           </div>
         </div>
